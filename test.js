7a2f69c38793ad5d00ed25f25152d69694a7e6f1
obovoid <git.obovoid@gmail.com>
2023-10-22 14:28:41 +0200

    Changed TypedFunction, 2nd TypedFunction concept

--- test.js
+++ test.js
@@ -1,32 +1,47 @@
-// First Function Body Concept
-const globals = {}
 
-function TypedFunction(name, func) {
-    const StringFunction = String(func);
-    
-    const functionOpening = StringFunction.search("{");
-    const sliceAndReduce = StringFunction.slice(functionOpening +1).replace(/(\r\n|\n|\r)/gm,"");
-    const split = sliceAndReduce.split("\"")
-    let retType;
-    split.every(str => {
-        if (str.startsWith('expect')) {
-            retType = str.replace("expect ", "");
-            return false
-        }
-        return true
-    });
+class TypedFunction {
+    #caller
+    #typeList = new Array();
+    constructor(...Arguments) {
+        let hasReturnType = false;
+        let returnType = null;
 
-    console.log(retType);
+        Arguments.forEach((regPam, index) => {
+            let regString = String(regPam);
+            if (!regString.startsWith('/') || !regString.endsWith('/')) throw new Error(`Failed to create Typed Function! The Parameter Syntax was invalid. If you have difficulties then please read the documentation.`);
+            let regStringSliced = regString.slice(1).slice(0, -1);
+            
+            const [name, type] = regStringSliced.split('@')
+
+            if (name === '->' && hasReturnType === true) throw new Error(`Failed to create Typed Function! It seems that you have two or more return Type defined while there can only be one return Type defined in a single function!`);
+            if (name === '->' && !hasReturnType) {
+                hasReturnType = true;
+                returnType = type
+
+                if (index !== Arguments.length -1) {
+                    // TODO, add Stacklog @obovoid
+                    console.warn(`It seems that you have defined a return type in a TypedFunction before submitting the last parameter. For better readability it is adviced to put the return type as the last parameter.`);
+                }
+            }
+
+            this.#typeList.push({name, type});
+        });
+    }
+
+    onCall = (callee) => {
+        this.#caller = callee
+    }
+
+    call() {
+        this.#caller();
+    }
 }
 
-TypedFunction("test", () => {
-    "expect Any"
-
+const Testname = new TypedFunction(/paramName@String/, /->@String/)
+Testname.onCall(result => {
     console.log("Hello World!");
 })
 
-function createFunction(funcName) {
-	if (funcName in globals) return console.error('Already exists');
-	globals[funcName] = new Function("return " + "function (a, b) { return a + b; }")();
-    
-}
+setTimeout(() => {
+    Testname.call();
+}, 500);
