de6edc235652ec8a22cfa62b870c4ece75e449b5
obovoid <git.obovoid@gmail.com>
2023-11-02 17:04:48 +0100

    added regions for better readability

--- typeSafeJS.js
+++ typeSafeJS.js
@@ -1,17 +1,12 @@
 
+/* #region  Caching  */
 // Normally all Environments have process included, but in case not we load an empty object on it.
 if (!process) {
     process = new Object();
 }
 
-if (!process.nextTick) {
-    process.nextTick = (cb) => {
-        if (typeof cb !== "function") throw new Error('Failed to setup nextTick. Expected Callback Function!');
-        setTimeout(() => {cb()}, 1)
-    }
-}
-
-process.nextTick(() => {
+// Setting to own scope, to avoid access from outside to the cache.
+{
     let _cache = []
     process.locateTypedValue = (functionHashRef, key) => {
         if (!_cache[functionHashRef] || !_cache[functionHashRef][key]) return '[Object TypedPromise]'
@@ -23,10 +18,10 @@ process.nextTick(() => {
         }
         _cache[functionHashRef][key] = value;
     }
-});
+}
+/* #endregion */
 
-
-
+/* #region  Functions */
 function matchElse(match, test, fallback) {
     return match === test || match === fallback || false
 }
@@ -34,12 +29,12 @@ function matchElse(match, test, fallback) {
 function _float(input) {
     return input.toFixed(1);
 }
+/* #endregion */
 
-const $TSJS_CACHE = []
-
+/* #region  Types */
 const TYPE = {
     getType: (value) => {
-        const type = Object.prototype.toString.call(value).split(' ')[1].replace(']' , '');
+        const type = Object.prototype.toString.call(value).split(' ')[1].replace(']', '');
         switch (type) {
             case 'Number':
                 return Number(value) === value && value % 1 !== 0 ? 'Float' : 'Integer';
@@ -62,7 +57,7 @@ const TYPE = {
         if (!Number.isInteger(value.charAt(0)) && (value.charAt(1) === ':') && matchElse(value.charAt(2), '/', '\\')) return true // Abs. path with Partition
         if (matchElse(value.charAt(0), '/', '\\')) {
             // Test is absolute path
-            switch(value.charAt(1)) {
+            switch (value.charAt(1)) {
                 case '/':
                     // '/' is invalid to be used twice in a row
                     return false
@@ -111,112 +106,6 @@ const TYPE = {
     "REGEXP": "RegExp",
     "BOOLEAN": "Boolean",
     "ANY": "Any"
-}
-
-class FunctionIdentifier {
-    #hash
-    constructor() {
-        this.#hash = Date.now().toString(36) + Math.random().toString(36).substring(2, 12).padStart(12, 0);
-    }
-
-    get identifier() {
-        return this.#hash;
-    }
-}
-
-class Enum {
-    #data = {}
-    constructor(...Arguments) {
-        Arguments.forEach((arg, index) => {
-            this.#data[arg] = index
-        })
-    }
-
-    $(value) {
-        return this.#data[value]
-    }
-}
-
-class TypedLet {
-    // Disallow direct changes to the Typed behaving let
-    #value
-    #type
-    constructor(type, value) {
-        this.#type = TYPE[type.toUpperCase()]
-        if (!this.#type) throw new Error(`Failed to construct TypedLet. Entered Type {${type}} does not exist!`);
-        const valType = TYPE.getType(value)
-        if (this.#type !== valType && this.#type !== 'Any') {
-            const isAnyNumber = valType === 'Float' || valType === 'Integer'
-            if (isAnyNumber && this.#type !== 'Number') {
-                throw new Error(`Failed to construct TypedLet. Entered Type value {${valType}} does not match ${this.#type}!`);
-            }
-
-            const isPath = valType === 'Path'
-            if (isPath && typeof value !== 'string') {
-                throw new Error(`Failed to construct TypedLet. Entered Type value {${valType}} does not match ${this.#type}!`)
-            } else if (!isAnyNumber && !isPath) {
-                throw new Error(`Failed to construct TypedLet. Entered Type value {${valType}} does not match ${this.#type}!`)
-            }
-        }
-        this.#value = value
-    }
-    
-    get value() {
-        if (this.#type === "Float" && typeof this.#value === "string") return Number(this.#value); 
-        return this.#value
-    }
-
-    get type() {
-        return this.#type
-    }
-
-    set value(val) {
-        const type = TYPE.getType(val);
-        if (this.#type !== 'Any') {
-            if (type !== this.#type) {
-                const isIntegerOrFloat = type === 'Float' || type === 'Integer'
-                const errSyntax = `Failed to set TypedLet. Entered value {type:${type}} does not match constructed type! {${this.#type}}`
-
-                if (this.#type === 'Number' && !isIntegerOrFloat) {
-                    throw new Error(errSyntax);   
-                }
-
-                if (this.#type === 'Integer') {
-                    throw new Error(errSyntax);   
-                }
-
-                if (this.#type === 'Path') {
-                    throw new Error(errSyntax);   
-                }
-                
-                if (this.#type === 'Function') {
-                    throw new Error(errSyntax);   
-                }
-
-                if (this.#type === 'Array') {
-                    throw new Error(errSyntax);
-                }
-
-                if (this.#type === 'Object') {
-                    throw new Error(errSyntax);
-                }
-
-                if (this.#type === 'String' && type !== 'Path') {
-                    throw new Error(errSyntax);
-                }
-                
-                if (this.#type === 'RegExp') {
-                    throw new Error(errSyntax);
-                }
-                
-                if (this.#type === 'Boolean') {
-                    throw new Error(errSyntax);
-                }
-            }
-        }
-        this.#value = val
-        return this.#value
-    }
 }
 
 const integer = {
@@ -308,7 +197,121 @@ const any = {
         return "any"
     }
 }
+/* #endregion */
 
+/* #region  Helper Classes */
+class FunctionIdentifier {
+    #hash
+    constructor() {
+        this.#hash = Date.now().toString(36) + Math.random().toString(36).substring(2, 12).padStart(12, 0);
+    }
+
+    get identifier() {
+        return this.#hash;
+    }
+}
+/* #endregion */
+
+/* #region  Enum, needs work! */
+class Enum {
+    #data = {}
+    constructor(...Arguments) {
+        Arguments.forEach((arg, index) => {
+            this.#data[arg] = index
+        })
+    }
+
+    $(value) {
+        return this.#data[value]
+    }
+}
+/* #endregion */
+
+/* #region  TypedLet */
+class TypedLet {
+    // Disallow direct changes to the Typed behaving let
+    #value
+    #type
+    constructor(type, value) {
+        this.#type = TYPE[type.toUpperCase()]
+        if (!this.#type) throw new Error(`Failed to construct TypedLet. Entered Type {${type}} does not exist!`);
+        const valType = TYPE.getType(value)
+        if (this.#type !== valType && this.#type !== 'Any') {
+            const isAnyNumber = valType === 'Float' || valType === 'Integer'
+            if (isAnyNumber && this.#type !== 'Number') {
+                throw new Error(`Failed to construct TypedLet. Entered Type value {${valType}} does not match ${this.#type}!`);
+            }
+
+            const isPath = valType === 'Path'
+            if (isPath && typeof value !== 'string') {
+                throw new Error(`Failed to construct TypedLet. Entered Type value {${valType}} does not match ${this.#type}!`)
+            } else if (!isAnyNumber && !isPath) {
+                throw new Error(`Failed to construct TypedLet. Entered Type value {${valType}} does not match ${this.#type}!`)
+            }
+        }
+        this.#value = value
+    }
+
+    get value() {
+        if (this.#type === "Float" && typeof this.#value === "string") return Number(this.#value);
+        return this.#value
+    }
+
+    get type() {
+        return this.#type
+    }
+
+    set value(val) {
+        const type = TYPE.getType(val);
+        if (this.#type !== 'Any') {
+            if (type !== this.#type) {
+                const isIntegerOrFloat = type === 'Float' || type === 'Integer'
+                const errSyntax = `Failed to set TypedLet. Entered value {type:${type}} does not match constructed type! {${this.#type}}`
+
+                if (this.#type === 'Number' && !isIntegerOrFloat) {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'Integer') {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'Path') {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'Function') {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'Array') {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'Object') {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'String' && type !== 'Path') {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'RegExp') {
+                    throw new Error(errSyntax);
+                }
+
+                if (this.#type === 'Boolean') {
+                    throw new Error(errSyntax);
+                }
+            }
+        }
+        this.#value = val
+        return this.#value
+    }
+}
+/* #endregion */
+
+/* #region  TypedFunction */
 class TypedFunction {
     #caller
     #typeList = new Array();
@@ -327,22 +330,22 @@ class TypedFunction {
                 let regString = String(regPam);
                 if (!regString.startsWith('/') || !regString.endsWith('/')) throw new Error(`Failed to create Typed Function! The Parameter Syntax was invalid. If you have difficulties then please read the documentation.`);
                 let regStringSliced = regString.slice(1).slice(0, -1);
-                
+
                 const [name, type] = regStringSliced.split('@')
-    
+
                 if (name === '->' && hasReturnType === true) throw new Error(`Failed to create Typed Function! It seems that you have two or more return Type defined while there can only be one return Type defined in a single function!`);
                 if (name === '->' && !hasReturnType) {
                     hasReturnType = true;
                     this.#returnType = type
 
-                    if (index !== Arguments.length -1 && typeof Arguments[index +1] !== "function") {
+                    if (index !== Arguments.length - 1 && typeof Arguments[index + 1] !== "function") {
                         console.warn(`It seems that you have defined a return type in a TypedFunction before submitting the last parameter. For better readability it is adviced to put the return type as the last parameter.`);
                     }
                 }
-    
-                this.#typeList.push({name, type});
+
+                this.#typeList.push({ name, type });
             } else {
-                if (index !== Arguments.length -1) {
+                if (index !== Arguments.length - 1) {
                     throw new Error(`It seems that you have defined a callback function in a TypedFunction which must be the last parameter but isn't!`);
                 }
 
@@ -385,7 +388,7 @@ class TypedFunction {
                                 // throw error? Type not specified
                             } else {
                                 let [value, type] = word.split(">>");
-                                
+
                                 try {
                                     eval(value)
                                 } catch (e) {
@@ -423,37 +426,40 @@ class TypedFunction {
     }
 
     call(...args) {
-        if ((this.#returnType !== null ? this.#typeList.length -1 : this.#typeList.length) > args.length) throw new Error(`Error calling Typed Function! Too few arguments! Expected ${this.#typeList.length} arguments but received: ${args.length}`);
+        if ((this.#returnType !== null ? this.#typeList.length - 1 : this.#typeList.length) > args.length) throw new Error(`Error calling Typed Function! Too few arguments! Expected ${this.#typeList.length} arguments but received: ${args.length}`);
 
         this.#typeList.forEach((typeObject, index) => {
             const name = typeObject.name
             const type = typeObject.type
-            
+
             if (name !== '->') { // return type
                 const argType = TYPE.getType(args[index]);
-                
+
                 if (type !== argType) throw new Error(`Error calling Typed Function! Expected ${type} for ${name} instead of ${argType}. Error in Call.`);
 
                 process.cacheValue(this.#id, name, args[index]);
-                
+
             }
         });
         this.#caller(...args);
     }
 }
+/* #endregion */
 
+/* #region  Testing Codespace */
 setTimeout(() => {
-    const Testname = new TypedFunction(/paramName@String/, /param2@Integer/, /param3@Integer/ , /->@String/, () => {
-        const str = paramName>>string
+    const Testname = new TypedFunction(/paramName@String/, /param2@Integer/, /param3@Integer/, /->@String/, () => {
+        const str = paramName >> string
         console.log(str);
 
-        let int = param2>>integer
+        let int = param2 >> integer
         console.log(int);
 
-        let int2 = param3>>integer
+        let int2 = param3 >> integer
         console.log(int2);
 
         console.log(int * int2);
     });
     Testname.call("Hello World", 5, 2);
-}, 10);
+}, 1);
+/* #endregion */
